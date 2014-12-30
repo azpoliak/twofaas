baef2d81f0678133c37ff39ffde15e1349ddf0ff
Gil Chen-Zion <gc2466@columbia.edu>
2014-12-30 14:39:37 +0200

    update package

--- plugins/pytwofaas/setup.py
+++ plugins/pytwofaas/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pytwofaas',
-      version='0.2',
+      version='0.3',
       description='Two Factor Authentication in Two Seconds',
       url='https://github.com/azpoliak/twofaas',
       author='Gil Chen-Zion',
