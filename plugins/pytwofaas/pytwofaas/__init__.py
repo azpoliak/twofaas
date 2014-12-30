0dee5d3fe2c57fb6a7dbddefdf356f4dd4f2e967
Gil Chen-Zion <gc2466@columbia.edu>
2014-12-30 14:35:47 +0200

    add to gitignore and update pytwofaas

--- plugins/pytwofaas/pytwofaas/__init__.py
+++ plugins/pytwofaas/pytwofaas/__init__.py
@@ -1,6 +1,15 @@
 import requests
 
-def init():
-    print "WOOT WOOT"
+class PyTwoFaas(companyToken):
 
-# def validate(clientId, code, companyToken):
+	url = "http://127.0.0.1:5000"
+
+	def init(clientId, phoneNum):
+    	payload = { 'userID' : clientId, 'compTK': companyToken, 'userNum': phoneNum }
+    	r = requests.post(url + "/init", data=payload)
+    	print(r.text)
+
+	def validate(clientId, code):
+		payload = { 'userID' : clientId, 'compTK': companyToken, 'twoAuth': code }
+		r = requests.post(url + "/validate", data=payload)
+    	print(r.text)
