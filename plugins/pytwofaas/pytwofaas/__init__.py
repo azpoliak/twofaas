6289503608af040779f92dcca036e6c4ef2b7a35
Gil Chen-Zion <gc2466@columbia.edu>
2014-12-30 12:39:49 +0200

    Python module plugin started

---
+++ plugins/pytwofaas/pytwofaas/__init__.py
@@ -0,0 +1,6 @@
+import requests
+
+def init(clientId, phoneNumber, companyToken):
+    
+
+def validate(clientId, code, companyToken):
---
+++ plugins/pytwofaas/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pytwofaas',
+      version='0.1',
+      description='Two Factor Authentication in Two Seconds',
+      url='https://github.com/azpoliak/twofaas',
+      author='Gil Chen-Zion',
+      author_email='gil.chenzion@gmail.com',
+      license='MIT',
+      packages=['pytwofaas'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
