d1e50d1769a6891afd55df31d568c47f6e35b082
Gil Chen-Zion <gc2466@columbia.edu>
2014-12-31 10:44:28 +0200

    join driver and database for majority of functionality, update and select on get code

--- example.py
+++ example.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 @app.route('/') 
 def index():
     p = PyTwoFaas("6D6997D5-AC88-4747-847B-C5BB3AB047A9")
-	p.sendAuthSMS('gil.chenzion@aasasja.com', '+19736504192')
-	#+19736504192
-	#+972527482538
+    print p.sendAuthEmail('bobob', 'gil.chenzio@gmail.com')
+    #000d49
+    #+19736504192
+    #+972527482538
     return 'Index!'
 
 if __name__ == '__main__':
--- plugins/pytwofaas/setup.py
+++ plugins/pytwofaas/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='pytwofaas',
-      version='0.13',
-      description='Two Factor Authentication in Two Seconds',
+      version='0.14',
+      description='Two Factor Authentication in Two Minutes',
       url='https://github.com/azpoliak/twofaas',
       author='Gil Chen-Zion',
       author_email='gil.chenzion@gmail.com',
